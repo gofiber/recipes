1f62e49e17de8bfb531f72765f6fa1657a7d4373
Fenny <25108519+Fenny@users.noreply.github.com>
2020-03-04 14:48:04 +0100

    Create main.go

---
+++ websocket/main.go
@@ -0,0 +1,43 @@
+// 🚀 Fiber is an Express inspired web framework written in Go with 💖
+// 📌 API Documentation: https://fiber.wiki
+// 📝 Github Repository: https://github.com/gofiber/fiber
+
+package main
+
+import (
+    "log"
+    "github.com/gofiber/fiber"
+)
+
+func main() {
+    app := fiber.New()
+    
+    // Optional middleware
+    app.Use("/ws", func(c *fiber.Ctx) {
+        if c.Get("host") != "localhost:3000" {
+            c.Status(403).Send("Request origin not allowed")
+        } else {
+            c.Next()
+        }
+    })
+    
+    // Upgraded websocket request
+    app.WebSocket("/ws", func(c *fiber.Conn) {
+        for {
+            mt, msg, err := c.ReadMessage()
+            if err != nil {
+                log.Println("read:", err)
+                break
+            }
+            log.Printf("recv: %s", msg)
+            err = c.WriteMessage(mt, msg)
+            if err != nil {
+                log.Println("write:", err)
+                break
+            }
+        }
+    })
+    
+    // ws://localhost:3000/ws
+    app.Listen(3000)
+}
