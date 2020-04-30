f36052d5d9d052eb7921b7ee6946118d09f5f3c8
Daniel "soro" Cunha <danielsoro@gmail.com>
2020-04-30 20:48:58 -0300

    Added comments

--- heroku/main.go
+++ heroku/main.go
@@ -1,3 +1,7 @@
+// 🚀 Fiber is an Express inspired web framework written in Go with 💖
+// 📌 API Documentation: https://fiber.wiki
+// 📝 Github Repository: https://github.com/gofiber/fiber
+
 package main
 
 import (
@@ -8,18 +12,24 @@ import (
 )
 
 func main() {
+
+	// Create new Fiber instance
 	app := fiber.New()
 
+	// Create new GET route
 	app.Get("/", func(ctx *fiber.Ctx) {
 		ctx.Send("Hello Heroku")
 	})
 
+	// Get the PORT from heroku env
 	port := os.Getenv("PORT")
 
+	// Verify if heroku provided the port or not
 	if port == "" {
 		port = "3000"
 		log.Print("$PORT == 3000")
 	}
 
+	// Start server on http://${heroku-url}:${port}
 	app.Listen(port)
 }
