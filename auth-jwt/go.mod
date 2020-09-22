7cb33d5810b504fe2afd316ca5798e971c81b680
Martin Lukasik <47029964+MartinLukasik@users.noreply.github.com>
2020-09-22 12:34:04 +0200

    auth-jwt: fixed type errors (#56)
    introduced while migrating to v2

--- auth-jwt/go.mod
+++ auth-jwt/go.mod
@@ -6,8 +6,9 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gofiber/cors v0.2.0 // indirect
 	github.com/gofiber/fiber v1.11.1 // indirect
-	github.com/gofiber/fiber/v2 v2.0.0-20200914103805-f3434e503771
+	github.com/gofiber/fiber/v2 v2.0.0
 	github.com/gofiber/jwt v0.1.0 // indirect
+	github.com/gofiber/jwt/v2 v2.0.0
 	github.com/gofiber/logger v0.2.2 // indirect
 	github.com/jinzhu/gorm v1.9.12
 	github.com/joho/godotenv v1.3.0
--- auth-jwt/middleware/auth.go
+++ auth-jwt/middleware/auth.go
@@ -8,17 +8,18 @@ import (
 )
 
 // Protected protect routes
-func Protected() func(*fiber.Ctx) {
+func Protected() fiber.Handler {
 	return jwtware.New(jwtware.Config{
 		SigningKey:   []byte(config.Config("SECRET")),
 		ErrorHandler: jwtError,
 	})
 }
 
-func jwtError(c *fiber.Ctx, err error) {
+func jwtError(c *fiber.Ctx, err error) error {
 	if err.Error() == "Missing or malformed JWT" {
-		c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"status": "error", "message": "Missing or malformed JWT", "data": nil})
-	} else {
-		c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"status": "error", "message": "Invalid or expired JWT", "data": nil})
+		return c.Status(fiber.StatusBadRequest).
+			JSON(fiber.Map{"status": "error", "message": "Missing or malformed JWT", "data": nil})
 	}
+	return c.Status(fiber.StatusUnauthorized).
+		JSON(fiber.Map{"status": "error", "message": "Invalid or expired JWT", "data": nil})
 }
