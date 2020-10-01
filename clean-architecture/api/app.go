3ac3c33e128aa86a92da605144bf19c30b063042
Hemanth Krishna <hkpdev008@gmail.com>
2020-10-02 00:46:42 +0530

    add: clean-architecture

---
+++ clean-architecture/api/app.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"clean-architecture/api/routes"
+	"clean-architecture/pkg/book"
+	"context"
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"time"
+)
+
+func main() {
+	db, err := DatabaseConnection()
+	if err != nil {
+		log.Fatal("Database Connection Error $s", err)
+	}
+	fmt.Println("Database connection success!")
+	bookCollection := db.Collection("books")
+	bookRepo := book.NewRepo(bookCollection)
+	bookService := book.NewService(bookRepo)
+
+	app := fiber.New()
+	app.Use(cors.New())
+	app.Get("/", func(ctx *fiber.Ctx) error {
+		return ctx.Send([]byte("Welcome to the clean-architecture mongo book shop!"))
+	})
+	api := app.Group("/api")
+	routes.BookRouter(api, bookService)
+	_ = app.Listen(":8080")
+
+}
+
+func DatabaseConnection() (*mongo.Database, error) {
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://username:password@localhost:27017/fiber"))
+	if err != nil {
+		return nil, err
+	}
+	db := client.Database("books")
+	return db, nil
+}
