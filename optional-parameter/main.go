498d9cbdddc5b03c277a6a4b67eb46b2c2aacf34
Lucas Turci <lucas.turci@gmail.com>
2020-10-03 15:38:10 -0300

    Add optional parameter example

---
+++ optional-parameter/main.go
@@ -0,0 +1,33 @@
+// ⚡️ Fiber is an Express inspired web framework written in Go with ☕️
+// 🤖 Github Repository: https://github.com/gofiber/fiber
+// 📌 API Documentation: https://docs.gofiber.io
+
+package main
+
+import (
+	"log"
+	"strconv"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+
+	// user list
+	users := [...]string{"Alice", "Bob", "Charlie", "David"}
+
+	// Fiber instance
+	app := fiber.New()
+
+	// Route to profile
+	app.Get("/:id?", func(c *fiber.Ctx) error {
+		id, err := strconv.Atoi(c.Params("id")) // transform id to array index
+		if err != nil || id < 0 || id >= len(users) {
+			return c.SendStatus(404) // invalid parameter returns 404
+		}
+		return c.SendString("Hello, " + users[id] + "!") // custom hello message to user with the id
+	})
+
+	// Start server
+	log.Fatal(app.Listen(":3000"))
+}
