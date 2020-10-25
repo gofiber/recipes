ce9ba8b686a1ee75ad14fbbcaf54ed3f39d683a2
Muhammad Fahriansyah <fachryansyah123@gmail.com>
2020-10-25 16:28:29 -0700

    fix: no datatype for return values on jwt middleware (#75)

--- jwt/middleware/auth.go
+++ jwt/middleware/auth.go
@@ -2,18 +2,18 @@ package middleware
 
 import (
 	"github.com/gofiber/fiber/v2"
-	jwtware "github.com/gofiber/jwt"
+	jwtware "github.com/gofiber/jwt/v2"
 )
 
 // Protected protect routes
-func Protected() func(*fiber.Ctx) {
+func Protected() func(*fiber.Ctx) error {
 	return jwtware.New(jwtware.Config{
 		SigningKey:   []byte("secret"),
 		ErrorHandler: jwtError,
 	})
 }
 
-func jwtError(c *fiber.Ctx, err error) {
+func jwtError(c *fiber.Ctx, err error) error {
 	if err.Error() == "Missing or malformed JWT" {
 		c.Status(fiber.StatusBadRequest)
 		return c.JSON(fiber.Map{"status": "error", "message": "Missing or malformed JWT", "data": nil})
