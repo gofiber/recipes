ed272d18985ad52f6f56c68cc740cb8c0e0c2ec5
sachin <c4c5nf3@gmail.com>
2020-12-29 20:51:29 +0800

    🚧 Housekeeping

--- firebase-auth/main.go
+++ firebase-auth/main.go
@@ -18,7 +18,7 @@ import (
 )
 
 func init() {
-	// loads values from .env into the system
+	// Loads values from .env into the system
 	if err := godotenv.Load(); err != nil {
 		log.Print("No .env file found")
 	}
@@ -28,70 +28,69 @@ func main() {
 	// Fiber instance
 	app := fiber.New()
 
-	// get firauth admin sdk json from .env
-	file, fileExi := os.LookupEnv("GOOGLE_APPLICATION_CREDENTIALS")
+	// Get google service account credentials
+	serviceAccount, fileExi := os.LookupEnv("GOOGLE_SERVICE_ACCOUNT")
 
 	if !fileExi {
 		log.Fatalf("Please provide valid firebbase auth credential json!")
 	}
 
-	// 2) create firebase app
-	opt := option.WithCredentialsFile(file)
+	// Initialize the firebase app.
+	opt := option.WithCredentialsFile(serviceAccount)
 	fireApp, _ := firebase.NewApp(context.Background(), nil, opt)
 
+	// Unauthenticated routes
+	app.Get("/salaanthe", salanthe)
+
+	// Initialize the middleware with config. See https://github.com/sacsand/gofiber-firebaseauth for more configuration options.
 	app.Use(gofiberfirebaseauth.New(gofiberfirebaseauth.Config{
 		// Firebase Authentication App Object
 		// Mandatory
 		FirebaseApp: fireApp,
-		// Ignore urls array
-		// Optional
-		IgnoreUrls: []string{"GET::/salut", "POST::/testauth", "GET::/user/:id"},
+		// Ignore urls array.
+		// Optional. These url will ignore by middleware
+		IgnoreUrls: []string{"GET::/salut", "POST::/ciao"},
 	}))
 
-	// Routes
+	// Authenticaed Routes.
 	app.Get("/hello", hello)
-	app.Get("/salut", salut)
-	app.Post("/ciao", ciao)
-	app.Post("/ciao", createCiao)
-	app.Get("/user/:id", ayubowan)
+	app.Get("/salut", salut)      // Ignore the auth by IgnoreUrls config
+	app.Post("/ciao", createCiao) // Ignore the auth by IgnoreUrls config
+	app.Get("/ayubowan", ayubowan)
 
-	// Start server
+	// Start server.
 	log.Fatal(app.Listen(":3001"))
 }
+
+/**
+*
+* Controllers
+*
+ */
 
 func hello(c *fiber.Ctx) error {
 	return c.SendString("Hello, World 👋!")
 }
 
 func salut(c *fiber.Ctx) error {
-	return c.SendString("Hello, World 👋!")
+	return c.SendString("Salut, World 👋!")
 }
 
 func ciao(c *fiber.Ctx) error {
-	return c.SendString("Hello, World 👋!")
+	return c.SendString("Ciao, World 👋! ")
 }
 
 func createCiao(c *fiber.Ctx) error {
-	return c.SendString("Hello, World 👋!")
+	return c.SendString("Create Cuiao, World 👋! i am a post")
 }
 
 func ayubowan(c *fiber.Ctx) error {
-	url := c.Method() + "::" + c.Path()
+	// Get authenticated user from context.
+	claims := c.Locals("user")
+	fmt.Println(claims)
+	return c.SendString("Ayubowan👋! ")
+}
 
-	fmt.Println(url)
-	fmt.Println(c.Path())
-
-	fmt.Println(c.OriginalURL())
-	r := c.Route()
-	fmt.Println(r.Method, r.Path)
-	url = r.Method + "::" + r.Path
-
-	// Experimental :: IF url contain any parms or querry
-	// if c.Path() != c.OriginalURL() {
-	// 	r := c.Route()
-	// 	fmt.Println(r.Method, r.Path, r.Params, r.Handlers)
-	// 	url = r.Method + "::" + r.Path
-	// }
-
-	return c.SendString("Ayubowan👋! " + url)
+func salanthe(c *fiber.Ctx) error {
+	return c.SendString("Salanthe👋! ")
 }
