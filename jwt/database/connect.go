5552e033b0e512cf6a895e54faf5a268d0ccb7b8
Vic Shóstak <vikkyshostak@gmail.com>
2021-03-16 08:44:29 +0300

    Fix code and dependencies of recipe

--- jwt/database/connect.go
+++ jwt/database/connect.go
@@ -17,9 +17,18 @@ func ConnectDB() {
 	var err error
 	p := config.Config("DB_PORT")
 	port, err := strconv.ParseUint(p, 10, 32)
+	if err != nil {
+		panic(err)
+	}
 
-	DB, err = gorm.Open("postgres", fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", config.Config("DB_HOST"), port, config.Config("DB_USER"), config.Config("DB_PASSWORD"), config.Config("DB_NAME")))
-
+	DB, err = gorm.Open("postgres", fmt.Sprintf(
+		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		config.Config("DB_HOST"),
+		port,
+		config.Config("DB_USER"),
+		config.Config("DB_PASSWORD"),
+		config.Config("DB_NAME"),
+	))
 	if err != nil {
 		panic("failed to connect database")
 	}
