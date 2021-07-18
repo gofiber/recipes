60c5d7e5d80716c044b3d6e09ddfb98b793993fa
lauslim12 <nicholasdwiarto@yahoo.com>
2021-07-18 14:20:08 +0700

    test: Update tests

--- react-fiber/web/src/App.test.tsx
+++ react-fiber/web/src/App.test.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders react text", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/react page/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders fiber text", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn fiber/i);
   expect(linkElement).toBeInTheDocument();
 });
