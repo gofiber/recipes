2b1004110d2ae32ccd7fd5cf99c535f49892646c
nawawishkid <nawawish.samerpark@gmail.com>
2021-07-21 22:40:43 +0700

    set server selection timeout to fail early

--- clean-architecture/api/app.go
+++ clean-architecture/api/app.go
@@ -37,7 +37,7 @@ func main() {
 
 func DatabaseConnection() (*mongo.Database, error) {
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://username:password@localhost:27017/fiber"))
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://username:password@localhost:27017/fiber").SetServerSelectionTimeout(5*time.Second))
 	if err != nil {
 		return nil, err
 	}
