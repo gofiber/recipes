537c57cc67e3ffbb1225936a0ebe0674f9a7e99a
lauslim12 <nicholasdwiarto@yahoo.com>
2021-08-11 20:57:46 +0700

    feat: Add 'created' and 'modified' attributes

--- docker-mariadb-clean-arch/scripts/migrations.sql
+++ docker-mariadb-clean-arch/scripts/migrations.sql
@@ -8,18 +8,20 @@ CREATE TABLE users (
   id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(255) NOT NULL,
   address VARCHAR(255) NOT NULL,
+  created VARCHAR(255) NOT NULL,
+  modified VARCHAR(255) NOT NULL,
   PRIMARY KEY (id)
 ) ENGINE=InnoDB CHARACTER SET utf8;
 
 -- Populate table with 10 users.
 INSERT INTO users VALUES
-(1, 'Sayu Ogiwara', 'Hokkaido, Japan'),
-(2, 'Chizuru Ichinose', 'Tokyo, Japan'),
-(3, 'Asagi Aiba', 'Kyoto, Japan'),
-(4, 'Rin Tohsaka', 'Kobe, Japan'),
-(5, 'Mai Sakurajima', 'Fujisawa, Japan'),
-(6, 'Aki Adagaki', 'Fukuoka, Japan'),
-(7, 'Asuna Yuuki', 'Shinagawa, Japan'),
-(8, 'Ruka Sarashina', 'Gotenba, Japan'),
-(9, 'Miyuki Shiba', 'Nagano, Japan'),
-(10, 'Fumino Furuhashi', 'Niigata, Japan');
+(1, 'Sayu Ogiwara', 'Hokkaido, Japan', UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),
+(2, 'Chizuru Ichinose', 'Tokyo, Japan', UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),
+(3, 'Asagi Aiba', 'Kyoto, Japan', UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),
+(4, 'Rin Tohsaka', 'Kobe, Japan', UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),
+(5, 'Mai Sakurajima', 'Fujisawa, Japan', UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),
+(6, 'Aki Adagaki', 'Fukuoka, Japan', UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),
+(7, 'Asuna Yuuki', 'Shinagawa, Japan', UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),
+(8, 'Ruka Sarashina', 'Gotenba, Japan', UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),
+(9, 'Miyuki Shiba', 'Nagano, Japan', UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),
+(10, 'Fumino Furuhashi', 'Niigata, Japan', UNIX_TIMESTAMP(), UNIX_TIMESTAMP());
