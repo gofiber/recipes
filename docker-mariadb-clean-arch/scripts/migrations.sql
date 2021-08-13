ccdddecf565dd915fd950559e1ab3e8d0bf047c4
lauslim12 <nicholasdwiarto@yahoo.com>
2021-08-13 12:59:39 +0700

    chore: Add sample data for migrations

--- docker-mariadb-clean-arch/scripts/migrations.sql
+++ docker-mariadb-clean-arch/scripts/migrations.sql
@@ -25,3 +25,22 @@ INSERT INTO users VALUES
 (8, 'Ruka Sarashina', 'Gotenba, Japan', UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),
 (9, 'Miyuki Shiba', 'Nagano, Japan', UNIX_TIMESTAMP(), UNIX_TIMESTAMP()),
 (10, 'Fumino Furuhashi', 'Niigata, Japan', UNIX_TIMESTAMP(), UNIX_TIMESTAMP());
+
+-- Create another sample table.
+CREATE TABLE cities (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  created VARCHAR(255) NOT NULL,
+  modified VARCHAR(255) NOT NULL,
+  user INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user) REFERENCES users (id)
+) ENGINE=InnoDB CHARACTER SET utf8;
+
+-- Create five data samples.
+INSERT INTO cities VALUES
+(1, 'Hokkaido', UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 1),
+(2, 'Tokyo', UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 1),
+(3, 'Kyoto', UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 1),
+(4, 'Osaka', UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 2),
+(5, 'Fukuoka', UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 3);
