ff65b518f90088636b7f74cab4a46290cbfe22c2
Mark Hähnel <hello@markhaehnel.de>
2022-06-15 16:03:04 +0200

    chore: fix review notes

--- template-asset-bundling/handlers/handlers.go
+++ template-asset-bundling/handlers/handlers.go
@@ -4,16 +4,19 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+// Renders the home view
 func Home(c *fiber.Ctx) error {
 	return c.Render("index", fiber.Map{
 		"Title": "Hello, World!",
 	})
 }
 
+// Renders the about view
 func About(c *fiber.Ctx) error {
 	return c.Render("about", nil)
 }
 
+// Renders the 404 view
 func NotFound(c *fiber.Ctx) error {
 	return c.Status(404).Render("404", nil)
 }
