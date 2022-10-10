0da71bf2d28cff520be4d6788c20f55c1410f3ed
Marc schulz <marcleonschulz@gmail.com>
2022-10-10 19:25:54 +0200

    Add a simple API in a docker container.

---
+++ docker-hello-world/go.mod
@@ -0,0 +1,13 @@
+module docker-hello-world
+
+go 1.18
+
+require (
+	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/gofiber/fiber/v2 v2.38.1 // indirect
+	github.com/klauspost/compress v1.15.11 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.40.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/sys v0.0.0-20221010170243-090e33056c14 // indirect
+)
---
+++ docker-hello-world/main.go
@@ -0,0 +1,27 @@
+// ⚡️ Fiber is an Express inspired web framework written in Go with ☕️
+// 🤖 Github Repository: https://github.com/gofiber/fiber
+// 📌 API Documentation: https://docs.gofiber.io
+
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	// Fiber instance
+	app := fiber.New()
+
+	// Routes
+	app.Get("/", hello)
+
+	// Start server
+	log.Fatal(app.Listen(":3000"))
+}
+
+// Handler
+func hello(c *fiber.Ctx) error {
+	return c.SendString("Hello, World 👋!")
+}
