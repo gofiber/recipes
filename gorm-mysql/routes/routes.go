34d272617c530ed740010093ae59fee46798fd88
Juan Calderon-Perez <jgcalderonperez@protonmail.com>
2023-06-06 22:45:55 -0400

    Fix hound warnings

--- gorm-mysql/routes/routes.go
+++ gorm-mysql/routes/routes.go
@@ -8,12 +8,12 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-// Hello
+//Hello
 func Hello(c *fiber.Ctx) error {
 	return c.SendString("fiber")
 }
 
-// AddBook
+//AddBook
 func AddBook(c *fiber.Ctx) error {
 	book := new(models.Book)
 	if err := c.BodyParser(book); err != nil {
@@ -33,7 +33,7 @@ func GetBook(c *fiber.Ctx) error {
 	return c.Status(200).JSON(books)
 }
 
-// AllBooks
+//AllBooks
 func AllBooks(c *fiber.Ctx) error {
 	books := []models.Book{}
 
@@ -42,7 +42,7 @@ func AllBooks(c *fiber.Ctx) error {
 	return c.Status(200).JSON(books)
 }
 
-// Update
+//Update
 func Update(c *fiber.Ctx) error {
 	book := new(models.Book)
 	if err := c.BodyParser(book); err != nil {
@@ -55,7 +55,7 @@ func Update(c *fiber.Ctx) error {
 	return c.Status(400).JSON("updated")
 }
 
-// Delete
+//Delete
 func Delete(c *fiber.Ctx) error {
 	book := new(models.Book)
 
