223934e3706d0f03ab081f293b091fca7aa4dcb6
Eeap <kdg97811@naver.com>
2023-07-05 03:23:45 +0900

    feat(Recipes): add new recipe ent ORM with MySQL

---
+++ ent-mysql/database/database.go
@@ -0,0 +1,23 @@
+package database
+
+import (
+	"http/ent"
+	"log"
+	"os"
+)
+
+var (
+	DBConn *ent.Client
+)
+
+func ConnectDb() {
+	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
+	dsn := "user:password@tcp(localhost:3306)/dbName?charset=utf8mb4&parseTime=True&loc=Local"
+	client, err := ent.Open("mysql", dsn)
+	if err != nil {
+		log.Fatal("Failed to connect to database \n", err)
+		os.Exit(1)
+	}
+	log.Println("Connect")
+	DBConn = client
+}
---
+++ ent-mysql/routes/routes.go
@@ -0,0 +1,68 @@
+package routes
+
+import (
+	"context"
+	"github.com/gofiber/fiber/v2"
+	"http/database"
+	"log"
+	"strconv"
+)
+
+var (
+	ctx = context.Background()
+)
+
+func GetBook(c *fiber.Ctx) error {
+	id, _ := strconv.Atoi(c.Params("id"))
+	b, err := database.DBConn.Book.
+		Get(ctx, id)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(err.Error())
+	}
+	log.Println("book data: ", b)
+	return c.Status(fiber.StatusOK).JSON(b)
+}
+func GetAllBook(c *fiber.Ctx) error {
+	b, err := database.DBConn.Book.
+		Query().All(ctx)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(err.Error())
+	}
+	log.Println("book data All: ", b)
+	return c.Status(fiber.StatusOK).JSON(b)
+}
+func CreateBook(c *fiber.Ctx) error {
+	b, err := database.DBConn.Book.
+		Create().
+		SetTitle("book").
+		SetAuthor("user").
+		Save(ctx)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(err.Error())
+	}
+	log.Println("book was created: ", b)
+	return c.Status(fiber.StatusOK).JSON(b)
+}
+func DeleteBook(c *fiber.Ctx) error {
+	id, _ := strconv.Atoi(c.Params("id"))
+	err := database.DBConn.Book.
+		DeleteOneID(id).
+		Exec(ctx)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(err.Error())
+	}
+	log.Println("book was deleted")
+	return c.Status(fiber.StatusOK).JSON(nil)
+}
+func UpdateBook(c *fiber.Ctx) error {
+	id, _ := strconv.Atoi(c.Params("id"))
+	b, err := database.DBConn.Book.
+		UpdateOneID(id).
+		SetTitle("updateBook").
+		Save(ctx)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(err.Error())
+	}
+	log.Println("book was updated: ", b)
+	return c.Status(fiber.StatusOK).JSON(b)
+}
