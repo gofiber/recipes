967d9a67fcf349e4e4fd058feddf804bb2097765
ugurkorkmaz <korkmaz.uu@gmail.com>
2023-07-06 13:58:48 +0300

    feat: create an `Entgo` client to connect to the `SQLite` database

---
+++ entgo-sveltekit/config/database/connect.go
@@ -0,0 +1,21 @@
+package database
+
+import (
+	"app/entity"
+	"context"
+	"errors"
+	"fmt"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func Connect(name string) (client *entity.Client, err error) {
+	client, err = entity.Open("sqlite3", fmt.Sprintf("file:%s?_fk=1", name))
+	if err != nil {
+		return nil, errors.New("failed opening connection to sqlite:" + err.Error())
+	}
+	if err := client.Schema.Create(context.Background()); err != nil {
+		return nil, errors.New("failed creating schema resources:" + err.Error())
+	}
+	return
+}
