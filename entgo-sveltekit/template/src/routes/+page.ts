8a95a6c96c293296c02195cd319969f97951ed67
ugurkorkmaz <korkmaz.uu@gmail.com>
2023-07-06 13:54:36 +0300

    feat: create web interface with `Sveltekit` and `Tailwindcss`, and add a Go code for generating the output (dist)

---
+++ entgo-sveltekit/template/src/routes/+page.ts
@@ -0,0 +1,17 @@
+import { PUBLIC_API_URL as uri } from '$env/static/public'
+import { error,loading,todos, type TodoType } from '$lib/store/todos';
+import type { PageLoad } from './$types';
+
+export const load = (async ({fetch}) => {
+    loading.set(true); // Set the loading state to true before fetching data
+    try {
+        const response = await fetch(uri + "/todo/list");
+        const result = await response.json() as TodoType[];
+        todos.set(result); // Set the retrieved data to the todos store
+    } catch (e) {
+        error.set(true); // Set the error state to true if an error occurs
+    } finally {
+        loading.set(false); // Set the loading state back to false after the fetch operation
+    }
+    return {};
+}) satisfies PageLoad;
---
+++ entgo-sveltekit/template/svelte.config.js
@@ -0,0 +1,19 @@
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/kit/vite";
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  preprocess: [vitePreprocess({})],
+
+  kit: {
+    adapter: adapter({
+      pages: "dist",
+      assets: "dist",
+      fallback: "index.html",
+      precompress: false,
+      strict: true,
+    }),
+  },
+};
+
+export default config;
