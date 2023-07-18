1c79a14c8377af7fc2556178584231e139aada66
ugurkorkmaz <korkmaz.uu@gmail.com>
2023-07-18 13:17:47 +0300

    feat: add Sveltekit embed app with static file serving

---
+++ sveltekit-embed/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"app/template"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/filesystem"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+const (
+	appName    = "Sveltekit Embed App"
+	apiVersion = "v1"
+	port       = ":3000"
+)
+
+func main() {
+	// Create new Fiber Instance
+	app := fiber.New(fiber.Config{
+		AppName: appName,
+	})
+	defer app.Shutdown()
+
+	// Middleware
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowHeaders: "*",
+	}))
+	app.Use(logger.New(logger.Config{
+		Format: "[${ip}]:${port} ${status} - ${method} ${path}\n",
+	}))
+	// Serve static files
+	app.All("/*", filesystem.New(filesystem.Config{
+		Root:         template.Dist(),
+		NotFoundFile: "index.html",
+		Index:        "index.html",
+	}))
+
+	// Start the server
+	if err := app.Listen(port); err != nil {
+		panic(err)
+	}
+}
