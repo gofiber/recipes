883fc18bcd6977a3b4933c921575f92ff8260d91
JiAn-Lim <limjian1990@gmail.com>
2023-09-22 12:00:41 +0900

    ✨ Add sqlboiler

---
+++ sqlboiler/api/controller/author_controller.go
@@ -0,0 +1,83 @@
+package controller
+
+import (
+	"context"
+	"fiber-sqlboiler/database"
+	"fiber-sqlboiler/models"
+	"strconv"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/volatiletech/sqlboiler/v4/boil"
+)
+
+func GetAuthors(c *fiber.Ctx) error {
+	authors, err := models.Authors().All(context.Background(), database.DB)
+	if err != nil {
+		return c.Status(500).JSON(err.Error())
+	}
+	return c.Status(200).JSON(authors)
+}
+
+func GetAuthor(c *fiber.Ctx) error {
+	id := c.Params("id")
+	authorId, err := strconv.Atoi(id)
+	if err != nil {
+		return c.Status(500).JSON(err.Error())
+	}
+	author, err := models.FindAuthor(context.Background(), database.DB, authorId)
+	if err != nil {
+		return c.Status(404).JSON(err.Error())
+	}
+	return c.Status(200).JSON(author)
+}
+
+func NewAuthor(c *fiber.Ctx) error {
+	author := models.Author{}
+	if err := c.BodyParser(&author); err != nil {
+		return c.Status(500).JSON(err.Error())
+	}
+	if err := author.Insert(context.Background(), database.DB, boil.Infer()); err != nil {
+		return c.Status(500).JSON(err.Error())
+	}
+	return c.Status(200).JSON(author)
+}
+
+func DeleteAuthor(c *fiber.Ctx) error {
+	id := c.Params("id")
+	authorId, err := strconv.Atoi(id)
+	if err != nil {
+		return c.Status(500).JSON(err.Error())
+	}
+	author, err := models.FindAuthor(context.Background(), database.DB, authorId)
+	if err != nil {
+		return c.Status(404).JSON(err.Error())
+	}
+	if _, err := author.Delete(context.Background(), database.DB); err != nil {
+		return c.Status(500).JSON(err.Error())
+	}
+	return c.SendStatus(200)
+}
+
+func UpdateAuthor(c *fiber.Ctx) error {
+	id := c.Params("id")
+	authorId, err := strconv.Atoi(id)
+	if err != nil {
+		return c.Status(500).JSON(err.Error())
+	}
+
+	newAuthor := models.Author{}
+	if err := c.BodyParser(&newAuthor); err != nil {
+		return c.Status(500).JSON(err.Error())
+	}
+
+	author, err := models.FindAuthor(context.Background(), database.DB, authorId)
+	if err != nil {
+		return c.Status(404).JSON(err.Error())
+	}
+
+	author.Name = newAuthor.Name
+	if _, err := author.Update(context.Background(), database.DB, boil.Infer()); err != nil {
+		return c.Status(500).JSON(err.Error())
+	}
+	return c.Status(200).JSON(author)
+}
