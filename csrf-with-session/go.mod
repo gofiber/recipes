b91464b0a66a0ffd8fcd080951655ddd02131e12
Jason McNeil <sixcolors@mac.com>
2023-10-14 17:00:54 -0300

    feat: add csrf-with-session example

---
+++ csrf-with-session/go.mod
@@ -0,0 +1,27 @@
+module main
+
+go 1.21.3
+
+require (
+	github.com/gofiber/fiber/v2 v2.49.3-0.20231013145519-cb89cce4ca08
+	github.com/gofiber/template/html/v2 v2.0.5
+	golang.org/x/crypto v0.14.0
+)
+
+require (
+	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/gofiber/template v1.8.2 // indirect
+	github.com/gofiber/utils v1.1.0 // indirect
+	github.com/google/uuid v1.3.1 // indirect
+	github.com/klauspost/compress v1.17.1 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/philhofer/fwd v1.1.2 // indirect
+	github.com/rivo/uniseg v0.4.4 // indirect
+	github.com/tinylib/msgp v1.1.8 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.50.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+)
---
+++ csrf-with-session/main.go
@@ -0,0 +1,193 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/csrf"
+	"github.com/gofiber/fiber/v2/middleware/session"
+	"github.com/gofiber/template/html/v2"
+	"golang.org/x/crypto/bcrypt"
+)
+
+// User represents a user in the dummy authentication system
+type User struct {
+	Username string
+	Password string
+}
+
+// Dummy user database
+var users = map[string]User{
+	"user1": {Username: "user1"},
+	"user2": {Username: "user2"},
+}
+
+func main() {
+	hashedPassword1, _ := bcrypt.GenerateFromPassword([]byte("password1"), 10)
+	hashedPassword2, _ := bcrypt.GenerateFromPassword([]byte("password2"), 10)
+	emptyHash, _ := bcrypt.GenerateFromPassword([]byte(""), 10) // used to help prevent timing attacks
+	emptyHashString := string(emptyHash)
+
+	users := map[string]User{
+		"user1": {Username: "user1", Password: string(hashedPassword1)},
+		"user2": {Username: "user2", Password: string(hashedPassword2)},
+	}
+	// HTML templates
+	engine := html.New("./views", ".html")
+
+	// Create a Fiber app
+	app := fiber.New(fiber.Config{
+		Views:       engine,
+		ViewsLayout: "layouts/main",
+	})
+
+	// Initialize a session manager
+	store := session.New()
+
+	// Configure the CSRF middleware
+	csrfConfig := csrf.Config{
+		Session:    store,
+		KeyLookup:  "form:csrf",
+		ContextKey: "csrf",
+	}
+	csrfMiddleware := csrf.New(csrfConfig)
+
+	// Route for the root path
+	app.Get("/", func(c *fiber.Ctx) error {
+		// render the root page as HTML
+		return c.Render("index", fiber.Map{
+			"Title": "Index",
+		})
+	})
+
+	// Route for the login page
+	app.Get("/login", csrfMiddleware, func(c *fiber.Ctx) error {
+		csrfToken, ok := c.Locals("csrf").(string)
+		if !ok {
+			return c.SendStatus(fiber.StatusInternalServerError)
+		}
+
+		return c.Render("login", fiber.Map{
+			"Title": "Login",
+			"csrf":  csrfToken,
+		})
+	})
+
+	// Route for processing the login
+	app.Post("/login", csrfMiddleware, func(c *fiber.Ctx) error {
+		// Retrieve the submitted form data
+		username := c.FormValue("username")
+		password := c.FormValue("password")
+
+		// Check if the credentials are valid
+		user, exists := users[username]
+		var checkPassword string
+		if exists {
+			checkPassword = user.Password
+		} else {
+			checkPassword = emptyHashString
+		}
+
+		if bcrypt.CompareHashAndPassword([]byte(checkPassword), []byte(password)) != nil {
+			// Authentication failed
+			csrfToken, ok := c.Locals("csrf").(string)
+			if !ok {
+				return c.SendStatus(fiber.StatusInternalServerError)
+			}
+
+			return c.Render("login", fiber.Map{
+				"Title": "Login",
+				"csrf":  csrfToken,
+				"error": "Invalid credentials",
+			})
+		}
+
+		// Set a session variable to mark the user as logged in
+		session, err := store.Get(c)
+		if err != nil {
+			return c.SendStatus(fiber.StatusInternalServerError)
+		}
+		if err := session.Reset(); err != nil {
+			return c.SendStatus(fiber.StatusInternalServerError)
+		}
+		session.Set("loggedIn", true)
+		if err := session.Save(); err != nil {
+			return c.SendStatus(fiber.StatusInternalServerError)
+		}
+
+		// Redirect to the protected route
+		return c.Redirect("/protected")
+	})
+
+	// Route for logging out
+	app.Get("/logout", func(c *fiber.Ctx) error {
+		// Retrieve the session
+		session, err := store.Get(c)
+		if err != nil {
+			return c.SendStatus(fiber.StatusInternalServerError)
+		}
+
+		// Revoke users authentication
+		if err := session.Destroy(); err != nil {
+			return c.SendStatus(fiber.StatusInternalServerError)
+		}
+
+		// Redirect to the login page
+		return c.Redirect("/login")
+	})
+
+	// Route for the protected content
+	app.Get("/protected", csrfMiddleware, func(c *fiber.Ctx) error {
+		// Check if the user is logged in
+		session, err := store.Get(c)
+		if err != nil {
+			return c.SendStatus(fiber.StatusInternalServerError)
+		}
+		loggedIn, _ := session.Get("loggedIn").(bool)
+
+		if !loggedIn {
+			// User is not authenticated, redirect to the login page
+			return c.Redirect("/login")
+		}
+
+		csrfToken, ok := c.Locals("csrf").(string)
+		if !ok {
+			return c.SendStatus(fiber.StatusInternalServerError)
+		}
+
+		return c.Render("protected", fiber.Map{
+			"Title": "Protected",
+			"csrf":  csrfToken,
+		})
+	})
+
+	// Route for processing the protected form
+	app.Post("/protected", csrfMiddleware, func(c *fiber.Ctx) error {
+		// Check if the user is logged in
+		session, err := store.Get(c)
+		if err != nil {
+			return c.SendStatus(fiber.StatusInternalServerError)
+		}
+		loggedIn, _ := session.Get("loggedIn").(bool)
+
+		if !loggedIn {
+			// User is not authenticated, redirect to the login page
+			return c.Redirect("/login")
+		}
+
+		csrfToken, ok := c.Locals("csrf").(string)
+		if !ok {
+			return c.SendStatus(fiber.StatusInternalServerError)
+		}
+
+		// Retrieve the submitted form data
+		message := c.FormValue("message")
+
+		return c.Render("protected", fiber.Map{
+			"Title":   "Protected",
+			"csrf":    csrfToken,
+			"message": message,
+		})
+	})
+
+	// Run the Fiber app on port 3000
+	_ = app.Listen(":3000")
+}
