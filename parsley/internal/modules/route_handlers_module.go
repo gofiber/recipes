e8b80b9d89cb30047a11aa3fb69291d782c2b9c7
Matthias Friedrich <1573457+matzefriedrich@users.noreply.github.com>
2024-09-02 22:40:56 +0200

    Updates comments for accuracy

--- parsley/internal/modules/route_handlers_module.go
+++ parsley/internal/modules/route_handlers_module.go
@@ -8,13 +8,20 @@ import (
 	"github.com/matzefriedrich/parsley/pkg/types"
 )
 
-// RegisterRouteHandlers Registers all route handlers of the GoFiber app.
+// RegisterRouteHandlers Registers all route handlers for the GoFiber app using the Parsley dependency injection framework.
+// It sets up the necessary features and registers individual route handlers.
+//
+// Parameters:
+// - registry: The ServiceRegistry instance that keeps track of service registrations.
+//
+// Returns:
+// - error: Any error that occurred during the registration process.
 func RegisterRouteHandlers(registry types.ServiceRegistry) error {
 
 	// The Parsley app requires a list of RouteHandler instances, so we must enable it.
 	features.RegisterList[route_handlers.RouteHandler](registry)
 
-	// RouteHandler implementations are going to be registered here.
+	// RouteHandler implementations are going to be registered here (add more as route handler registrations as needed)
 	registration.RegisterTransient(registry, route_handlers.NewGreeterRouteHandler)
 
 	return nil
