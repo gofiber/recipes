3f51014c34672efb2aec67c2a11206760ab18bdb
lee-lou2 <lee@lou2.kr>
2025-05-07 21:26:49 +0900

    Refactor: Log errors from Sentry initialization

--- aws-ses-sender/main.go
+++ aws-ses-sender/main.go
@@ -4,15 +4,18 @@ import (
 	"aws-ses-sender/api"
 	"aws-ses-sender/cmd"
 	"aws-ses-sender/config"
+	"log"
 
 	"github.com/getsentry/sentry-go"
 )
 
 func main() {
 	// Sentry
-	_ = sentry.Init(sentry.ClientOptions{
+	if err := sentry.Init(sentry.ClientOptions{
 		Dsn: config.GetEnv("SENTRY_DSN"),
-	})
+	}); err != nil {
+		log.Printf("Sentry initialization failed: %v", err)
+	}
 
 	// Message Scheduler
 	go cmd.RunScheduler()
